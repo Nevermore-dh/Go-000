25f2c2a63dc005a1f9bd61d9220f18f4e0d9b1b6
Van <nevermore_dh@sina.com>
2020-12-10 02:40:36 +0800

    week03 homework update

--- Week03/README.md
+++ Week03/README.md
@@ -1,4 +1,4 @@
 ### Week03 作业：
 Q: 基于 errgroup 实现一个 http server 的启动和关闭 ，以及 linux signal 信号的注册和处理，要保证能够 一个退出，全部注销退出。
 
-A: 
+A: Talk is cheap, code is also cheap..
--- Week03/main.go
+++ Week03/main.go
@@ -2,40 +2,70 @@ package main
 
 import (
 	"context"
+	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"golang.org/x/sync/errgroup"
+	"time"
 )
 
+type Server struct {
+	http *http.Server
+	ctx context.Context
+}
+
+func NewServer(addr string, mux http.Handler) *Server {
+	return &Server{
+		http: &http.Server{
+			Addr:         addr,
+			Handler:      mux,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		},
+		ctx: context.Background(),
+	}
+}
+
+func (s *Server)Start() error {
+	log.Println("Server start listening...")
+	return s.http.ListenAndServe()
+}
+
+func (s *Server)Stop() error {
+	log.Println("Server shutdown")
+	return s.http.Shutdown(s.ctx)
+}
+
 func main() {
-	eg := errgroup.Group{}
-	serErr := make(chan error, 1)
-	sg := make(chan os.Signal, 1)
+	eg, ctx := errgroup.WithContext(context.Background())
 
-	s := http.Server{Addr: ":8080"}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+	server := NewServer(":8080", mux)
 
 	eg.Go(func() error {
-		go func() {
-			serErr <- s.ListenAndServe()
-		}()
+		return server.Start()
+	})
+
+	eg.Go(func() error {
+		sg := make(chan os.Signal, 1)
+		signal.Notify(sg, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL)
 		select {
-		case err := <-serErr:
-			close(sg)
-			close(serErr)
-			return err
+		case <-sg:
+			log.Println("Received close signal")
+		case <-ctx.Done():
+			log.Printf("Server error: %v, context canceled\n", ctx.Err())
 		}
+		return server.Stop()
 	})
 
-	eg.Go(func() error {
-		signal.Notify(sg,
-			syscall.SIGINT|syscall.SIGTERM|syscall.SIGKILL)
-		<-sg
-		return s.Shutdown(context.TODO())
-	})
-
-	log.Println(eg.Wait())
+	if err := eg.Wait(); err != nil {
+		log.Printf("Server error: %v, exit\n", err)
+		return
+	}
+	log.Println("Server exit")
 }
